c1d5e55709c33066a37c06715cc22ff4d5a204a7
seval931 <svalencias@unal.edu.co>
2017-10-05 17:05:23 -0500

    json link variable name correction

--- js/fenController.js
+++ js/fenController.js
@@ -1,5 +1,6 @@
 var app = "";
-var jsonData=[{
+var jsonData=[
+{
 "no":"1",
 "etc":" Amazonas",
 "fecha":"11 DE AGOSTO",
@@ -7,7 +8,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"-3.775.072",
 "coordenaday":"-70.190.738",
-"link":"106904"
+"lin":"106904"
 },
 {
 "no":"2",
@@ -17,7 +18,7 @@ var jsonData=[{
 "estado":"POR DEFINIR",
 "coordenadax":"6.815.927",
 "coordenaday":"-75.426.080",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"3",
@@ -27,7 +28,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"7.896.890",
 "coordenaday":"-76.563.602",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"4",
@@ -37,7 +38,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"6.649.707",
 "coordenaday":"-71.122.013",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"5",
@@ -47,7 +48,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.515.978",
 "coordenaday":"-75.723.369",
-"link":"106905"
+"lin":"106905"
 },
 {
 "no":"6",
@@ -57,7 +58,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"10.740.820",
 "coordenaday":"-74.940.417",
-"link":"106906"
+"lin":"106906"
 },
 {
 "no":"7",
@@ -67,7 +68,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"10.988.865",
 "coordenaday":"-74.808.233",
-"link":"106907"
+"lin":"106907"
 },
 {
 "no":"8",
@@ -77,7 +78,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"6.335.632",
 "coordenaday":"-75.553.457",
-"link":"106909"
+"lin":"106909"
 },
 {
 "no":"9",
@@ -87,7 +88,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"10.988.865",
 "coordenaday":"74.808.233",
-"link":"106908"
+"lin":"106908"
 },
 {
 "no":"10",
@@ -97,7 +98,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.651.706",
 "coordenaday":"-74.094.083",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"11",
@@ -107,7 +108,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"9.175.307",
 "coordenaday":"-74.629.976",
-"link":"106910"
+"lin":"106910"
 },
 {
 "no":"12",
@@ -117,7 +118,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"5.603.707",
 "coordenaday":"-73.177.345",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"13",
@@ -127,7 +128,7 @@ var jsonData=[{
 "estado":"POR DEFINIR",
 "coordenadax":"7.165.358",
 "coordenaday":"-73.110.994",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"14",
@@ -137,7 +138,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"3.921.854",
 "coordenaday":"-76.970.704",
-"link":"106911"
+"lin":"106911"
 },
 {
 "no":"15",
@@ -147,7 +148,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"5.323.986",
 "coordenaday":"-75.416.268",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"16",
@@ -157,7 +158,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"3.450.924",
 "coordenaday":"-76.522.576",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"17",
@@ -167,7 +168,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"1",
 "coordenaday":"-74.006.858",
-"link":"106912"
+"lin":"106912"
 },
 {
 "no":"18",
@@ -177,7 +178,7 @@ var jsonData=[{
 "estado":"POR DEFINIR",
 "coordenadax":"10.391.399",
 "coordenaday":"-75.489.371",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"19",
@@ -187,7 +188,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.723.677",
 "coordenaday":"-75.923.086",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"20",
@@ -197,7 +198,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"5.486.887",
 "coordenaday":"-71.497.610",
-"link":"106913"
+"lin":"106913"
 },
 {
 "no":"21",
@@ -207,7 +208,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"2.364.316",
 "coordenaday":"-77.000.158",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"22",
@@ -217,7 +218,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"9.595.171",
 "coordenaday":"-73.552.219",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"23",
@@ -227,7 +228,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.863.390",
 "coordenaday":"-74.052.034",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"24",
@@ -237,7 +238,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"5.146.402",
 "coordenaday":"-76.803.013",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"25",
@@ -247,7 +248,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"11.008.298",
 "coordenaday":"-74.246.374",
-"link":"106914"
+"lin":"106914"
 },
 {
 "no":"26",
@@ -257,7 +258,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"8.439.250",
 "coordenaday":"-75.853.045",
-"link":"106915"
+"lin":"106915"
 },
 {
 "no":"27",
@@ -267,7 +268,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"8.141.635",
 "coordenaday":"-72.466.244",
-"link":"106916"
+"lin":"106916"
 },
 {
 "no":"28",
@@ -277,7 +278,7 @@ var jsonData=[{
 "estado":"POR DEFINIR",
 "coordenadax":"5.220.047",
 "coordenaday":"-74.182.339",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"29",
@@ -287,7 +288,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.830.768",
 "coordenaday":"-75.680.123",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"30",
@@ -297,7 +298,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"5.890.628",
 "coordenaday":"-73.063.855",
-"link":"106975"
+"lin":"106975"
 },
 {
 "no":"31",
@@ -307,7 +308,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"6.168.616",
 "coordenaday":"-75.587.079",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"32",
@@ -317,7 +318,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.842.440",
 "coordenaday":"-74.344.428",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"33",
@@ -327,7 +328,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"1.763.731",
 "coordenaday":"-75.570.294",
-"link":"106917"
+"lin":"106917"
 },
 {
 "no":"34",
@@ -337,7 +338,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"7.071.031",
 "coordenaday":"-73.106.217",
-"link":"106982"
+"lin":"106982"
 },
 {
 "no":"35",
@@ -347,7 +348,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.319.713",
 "coordenaday":"-74.402.246",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"36",
@@ -357,7 +358,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.303.349",
 "coordenaday":"-74.803.123",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"37",
@@ -367,7 +368,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"7.073.129",
 "coordenaday":"-73.169.203",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"38",
@@ -377,7 +378,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.054.3",
 "coordenaday":"-76.295.419",
-"link":"106986"
+"lin":"106986"
 },
 {
 "no":"39",
@@ -387,7 +388,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"2.971.477",
 "coordenaday":"-68.969.510",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"40",
@@ -397,7 +398,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"2.095.879",
 "coordenaday":"-72.100.510",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"41",
@@ -407,7 +408,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"2.749.019",
 "coordenaday":"-75.449.037",
-"link":"106989"
+"lin":"106989"
 },
 {
 "no":"42",
@@ -417,7 +418,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.441.213",
 "coordenaday":"-75.238.791",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"43",
@@ -427,7 +428,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"1",
 "coordenaday":"-77.439.853",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"44",
@@ -437,7 +438,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"6.169.447",
 "coordenaday":"-75.613.329",
-"link":"106992"
+"lin":"106992"
 },
 {
 "no":"45",
@@ -447,7 +448,7 @@ var jsonData=[{
 "estado":"CANCELADO FORO",
 "coordenadax":"3.261.615",
 "coordenaday":"-76.540.823",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"46",
@@ -457,7 +458,7 @@ var jsonData=[{
 "estado":"CANCELADO FORO",
 "coordenadax":"11.637.145",
 "coordenaday":"-72.265.589",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"47",
@@ -467,7 +468,7 @@ var jsonData=[{
 "estado":"POR DEFINIR",
 "coordenadax":"9.180.754",
 "coordenaday":"-75.914.405",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"48",
@@ -477,7 +478,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"9.240.748",
 "coordenaday":"-74.753.151",
-"link":"106993"
+"lin":"106993"
 },
 {
 "no":"49",
@@ -487,7 +488,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"10.343.595",
 "coordenaday":"-74.322.237",
-"link":"106994"
+"lin":"106994"
 },
 {
 "no":"50",
@@ -497,7 +498,7 @@ var jsonData=[{
 "estado":"POR DEFINIR",
 "coordenadax":"11.378.448",
 "coordenaday":"-72.248.203",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"51",
@@ -507,7 +508,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"10.856.062",
 "coordenaday":"-74.774.442",
-"link":"106995"
+"lin":"106995"
 },
 {
 "no":"52",
@@ -517,7 +518,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"5.077.517",
 "coordenaday":"-75.516.268",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"53",
@@ -527,7 +528,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"6.255.294",
 "coordenaday":"-75.564.904",
-"link":"106997"
+"lin":"106997"
 },
 {
 "no":"54",
@@ -537,7 +538,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"3.485.893",
 "coordenaday":"-73.116.660",
-"link":"106998"
+"lin":"106998"
 },
 {
 "no":"55",
@@ -547,7 +548,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"8.823.775",
 "coordenaday":"-75.782.826",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"56",
@@ -557,7 +558,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.678.858",
 "coordenaday":"-74.232.826",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"57",
@@ -567,7 +568,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"2.017.503",
 "coordenaday":"-77.954.097",
-"link":"106970"
+"lin":"106970"
 },
 {
 "no":"58",
@@ -577,7 +578,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"2.949.610",
 "coordenaday":"-75.265.950",
-"link":"106968"
+"lin":"106968"
 },
 {
 "no":"59",
@@ -587,7 +588,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"8.279.882",
 "coordenaday":"-72.935.462",
-"link":"106964"
+"lin":"106964"
 },
 {
 "no":"60",
@@ -597,7 +598,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"3.536.811",
 "coordenaday":"-76.296.627",
-"link":"107003"
+"lin":"107003"
 },
 {
 "no":"61",
@@ -607,7 +608,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"1.207.519",
 "coordenaday":"-77.285.892",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"62",
@@ -617,7 +618,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.796.639",
 "coordenaday":"-75.782.788",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"63",
@@ -627,7 +628,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"6.984.593",
 "coordenaday":"-73.021.266",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"64",
@@ -637,7 +638,7 @@ var jsonData=[{
 "estado":"POR DEFINIR",
 "coordenadax":"1.849.461",
 "coordenaday":"-76.104.193",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"65",
@@ -647,7 +648,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"2.482.825",
 "coordenaday":"-76.604.158",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"66",
@@ -657,7 +658,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"0.654.3",
 "coordenaday":"-76.139.419",
-"link":"106952"
+"lin":"106952"
 },
 {
 "no":"67",
@@ -667,7 +668,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"5.694.608",
 "coordenaday":"-76.650.499",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"68",
@@ -677,7 +678,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.482.536",
 "coordenaday":"-75.686.416",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"69",
@@ -687,7 +688,7 @@ var jsonData=[{
 "estado":"POR DEFINIR",
 "coordenadax":"11.537.384",
 "coordenaday":"-72.917.060",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"70",
@@ -697,7 +698,7 @@ var jsonData=[{
 "estado":"POR DEFINIR",
 "coordenadax":"6.145.946",
 "coordenaday":"-75.375.429",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"71",
@@ -707,7 +708,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"5.107.868",
 "coordenaday":"-75.982.958",
-"link":"106948"
+"lin":"106948"
 },
 {
 "no":"72",
@@ -717,7 +718,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"6.149.918",
 "coordenaday":"-75.616.863",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"73",
@@ -727,7 +728,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"8.830.242",
 "coordenaday":"-75.446.646",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"74",
@@ -737,7 +738,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"12.576.357",
 "coordenaday":"-79.303.789",
-"link":"106944"
+"lin":"106944"
 },
 {
 "no":"75",
@@ -747,7 +748,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"11.158.977",
 "coordenaday":"-73.934.921",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"76",
@@ -757,7 +758,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"6.815.745",
 "coordenaday":"-73.233.824",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"77",
@@ -767,7 +768,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"9.320.156",
 "coordenaday":"-75.436.479",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"78",
@@ -777,7 +778,7 @@ var jsonData=[{
 "estado":"POR DEFINIR",
 "coordenadax":"4.581.560",
 "coordenaday":"-74.211.804",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"79",
@@ -787,7 +788,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"5.717.724",
 "coordenaday":"-72.930.567",
-"link":"106949"
+"lin":"106949"
 },
 {
 "no":"80",
@@ -797,7 +798,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"10.915.492",
 "coordenaday":"-74.796.907",
-"link":"106936"
+"lin":"106936"
 },
 {
 "no":"81",
@@ -807,7 +808,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"9.308.861",
 "coordenaday":"-75.235.223",
-"link":"106933"
+"lin":"106933"
 },
 {
 "no":"82",
@@ -817,7 +818,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.198.023",
 "coordenaday":"-75.181.930",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"83",
@@ -827,7 +828,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.091.237",
 "coordenaday":"-76.195.938",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"84",
@@ -837,7 +838,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"1.783.378",
 "coordenaday":"-78.789.646",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"85",
@@ -847,7 +848,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"5.541.685",
 "coordenaday":"-73.355.727",
-"link":"106928"
+"lin":"106928"
 },
 {
 "no":"86",
@@ -857,7 +858,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"8.132.901",
 "coordenaday":"-76.599.093",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"87",
@@ -867,7 +868,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"11.717.000",
 "coordenaday":"-72.265.153",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"88",
@@ -877,7 +878,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"3.832.700",
 "coordenaday":"-76.438.807",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"89",
@@ -887,7 +888,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"10.473.042",
 "coordenaday":"-73.256.744",
-"link":"106922"
+"lin":"106922"
 },
 {
 "no":"90",
@@ -897,7 +898,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"1",
 "coordenaday":"-70.589.628",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"91",
@@ -907,7 +908,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"4.895.791",
 "coordenaday":"-69.609.599",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"92",
@@ -917,7 +918,7 @@ var jsonData=[{
 "estado":"POR DEFINIR",
 "coordenadax":"4.089.058",
 "coordenaday":"-73.507.620",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"93",
@@ -927,7 +928,7 @@ var jsonData=[{
 "estado":"POR DEFINIR",
 "coordenadax":"5.337.666",
 "coordenaday":"-72.397.610",
-"link":"-"
+"lin":"-"
 },
 {
 "no":"94",
@@ -937,7 +938,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"3.542.424",
 "coordenaday":"-76.495.542",
-"link":"106919"
+"lin":"106919"
 },
 {
 "no":"95",
@@ -947,7 +948,7 @@ var jsonData=[{
 "estado":"CONFIRMADA",
 "coordenadax":"5.035.445",
 "coordenaday":"-74.008.690",
-"link":"-"
+"lin":"-"
 }
 
 ]
